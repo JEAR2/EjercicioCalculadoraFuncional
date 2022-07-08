060aa5b5aa9e28d87e0130a06b87dd3fdcb4ef7f
JEAR2 <jhedacro@gmail.com>
2022-07-07 21:35:24 -0500

    Se agrega validaciones y se pasa el tipo de listas a Double

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Operations operations = new Operations();
-            List<Integer> listOne = List.of(1,2,3,10,25,6);
-            List<Integer> listTwo = List.of(1,5,3,4,4,6);
+            List<Double> listOne = List.of(1.0,2.0,3.0,10.0,25.0,6.0);
+            List<Double> listTwo = List.of(1.0,5.0,3.0,4.0,4.0,6.0);
         System.out.println("LISTAS");
         System.out.println(listOne.stream().collect(Collectors.toList()));
         System.out.println(listTwo.stream().collect(Collectors.toList()));
--- src/calculator/Operations.java
+++ src/calculator/Operations.java
@@ -1,24 +1,25 @@
 package calculator;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Operations {
-    public void suma(List<Integer> listOne, List<Integer> listTwo){
+    public void suma(List<Double> listOne, List<Double> listTwo){
         AtomicInteger indice = new AtomicInteger();
 
         System.out.println("SUMA: " + listOne.stream().map(element->element + listTwo.get(indice.getAndIncrement()))
                 .collect(Collectors.toList()));
     }
 
-    public void resta(List<Integer> listOne, List<Integer> listTwo){
+    public void resta(List<Double> listOne, List<Double> listTwo){
         AtomicInteger indice = new AtomicInteger();
         System.out.println("RESTA: "+listOne.stream().map(element->element - listTwo.get(indice.getAndIncrement()))
                 .collect(Collectors.toList()));
     }
 
-    public void multiplication(List<Integer> listOne, List<Integer> listTwo){
+    public void multiplication(List<Double> listOne, List<Double> listTwo){
         AtomicInteger indice = new AtomicInteger();
         System.out.println("MULTIPLICACIÓN: "+listOne.stream().map(element->element * listTwo.get(indice.getAndIncrement()))
                 .collect(Collectors.toList()));
@@ -26,7 +27,7 @@ public class Operations {
     }
 
 
-    public void division(List<Integer> listOne, List<Integer> listTwo){
+    public void division(List<Double> listOne, List<Double> listTwo){
 
         AtomicInteger indice = new AtomicInteger();
         System.out.println("DIVISIÓN: "+listTwo.stream()
@@ -40,7 +41,7 @@ public class Operations {
                 .collect(Collectors.toList()));
     }
 
-    public void modulo(List<Integer> listOne, List<Integer> listTwo){
+    public void modulo(List<Double> listOne, List<Double> listTwo){
         AtomicInteger indice = new AtomicInteger();
         System.out.println("MODULO: "+listTwo.stream()
                 .map(element-> {
